0444a3b6d5c07eee3d1566d5aec627d8ad618646
jackgris <jackgris2@gmail.com>
2012-11-24 19:58:50 -0300

    ultimo agregado mapa simple en una pestaña

--- src/com/aprendiendodeandroid/bancos/rosario/MapaGeneral.java
+++ src/com/aprendiendodeandroid/bancos/rosario/MapaGeneral.java
@@ -3,14 +3,21 @@ package com.aprendiendodeandroid.bancos.rosario;
 import android.os.Bundle;
 
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
 
 public class MapaGeneral extends MapActivity {
 
+	private MapView mapageneral;
+	
 	@Override
 	protected void onCreate(Bundle icicle) {
-		// TODO Auto-generated method stub
 		super.onCreate(icicle);
 		setContentView(R.layout.mapageneral);
+		
+		// tomo referencia del mapa
+		mapageneral = (MapView)findViewById(R.id.mapageneral);
+		
+		mapageneral.setBuiltInZoomControls(true);
 		
 	}
 	
