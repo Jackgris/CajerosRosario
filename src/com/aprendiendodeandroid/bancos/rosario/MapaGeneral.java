ba0c1d5673506073657be3dc419b3d9098def0b1
jackgris <jackgris2@gmail.com>
2012-12-23 01:13:40 -0300

    agregando la configurarcion del layout para el mapa y configurando nuestra clase

--- src/com/aprendiendodeandroid/bancos/rosario/MapaGeneral.java
+++ src/com/aprendiendodeandroid/bancos/rosario/MapaGeneral.java
@@ -2,30 +2,27 @@ package com.aprendiendodeandroid.bancos.rosario;
 
 import android.os.Bundle;
 
-import com.google.android.maps.MapActivity;
-import com.google.android.maps.MapView;
+public class MapaGeneral extends android.support.v4.app.FragmentActivity {
 
-public class MapaGeneral extends MapActivity {
-
-	private MapView mapageneral = null;
+//	private MapView mapageneral = null;
 	
 	@Override
 	protected void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
 		setContentView(R.layout.mapageneral);
 		
-		// tomo referencia del mapa
-		mapageneral = (MapView)findViewById(R.id.mapageneral);
-		
-		mapageneral.setBuiltInZoomControls(true);
-		mapageneral.setStreetView(true);
+//		// tomo referencia del mapa
+//		mapageneral = (MapView)findViewById(R.id.mapageneral);
+//		
+//		mapageneral.setBuiltInZoomControls(true);
+//		mapageneral.setStreetView(true);
 		
 	}
 	
-	@Override
-	protected boolean isRouteDisplayed() {
-		
-		return false;
-	}
+//	@Override
+//	protected boolean isRouteDisplayed() {
+//		
+//		return false;
+//	}
 
 }
