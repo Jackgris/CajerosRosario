6ecb48a183b503f3eab44d9742af11e2ea4cdb46
jackgris <jackgris2@gmail.com>
2012-12-23 18:38:44 -0300

    agregando algunos cambios para intentar hacer andar correctamente la API v2 Maps

--- src/com/aprendiendodeandroid/bancos/rosario/MapaGeneral.java
+++ src/com/aprendiendodeandroid/bancos/rosario/MapaGeneral.java
@@ -4,25 +4,9 @@ import android.os.Bundle;
 
 public class MapaGeneral extends android.support.v4.app.FragmentActivity {
 
-//	private MapView mapageneral = null;
-	
 	@Override
 	protected void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
-		setContentView(R.layout.mapageneral);
-		
-//		// tomo referencia del mapa
-//		mapageneral = (MapView)findViewById(R.id.mapageneral);
-//		
-//		mapageneral.setBuiltInZoomControls(true);
-//		mapageneral.setStreetView(true);
-		
+		setContentView(R.layout.mapageneral);	
 	}
-	
-//	@Override
-//	protected boolean isRouteDisplayed() {
-//		
-//		return false;
-//	}
-
 }
