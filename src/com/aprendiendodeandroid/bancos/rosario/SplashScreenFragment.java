36c1dab0b8ef377da0a9e337cd9953041fe73376
jackgris <jackgris2@gmail.com>
2013-01-05 10:05:23 -0300

    agregadas un par de librerias para realizar injeccion de dependencias y agregado el splashscreen usando roboguice

--- src/com/aprendiendodeandroid/bancos/rosario/SplashScreenFragment.java
+++ src/com/aprendiendodeandroid/bancos/rosario/SplashScreenFragment.java
@@ -1,27 +1,25 @@
 package com.aprendiendodeandroid.bancos.rosario;
 
-import com.aprendiendodeandroid.bancos.rosario.R;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import roboguice.fragment.RoboFragment;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class SplashScreenFragment extends RoboFragment {
+import roboguice.fragment.RoboFragment;
 
-	 @SuppressWarnings("unused")
-	private static final Logger log = LoggerFactory
-	            .getLogger(SplashScreenFragment.class);
-	    
-	 
-	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-	            Bundle savedInstanceState) {
-	        View view = inflater.inflate(R.layout.splashscreen, container,
-	                false);
-	        return view;
-	    }
+public class SplashScreenFragment extends RoboFragment{
+	
+	// Este objeto nos servira para tomar el log de errores
+	@SuppressWarnings("unused")
+	private static final Logger log = LoggerFactory.getLogger(SplashScreenFragment.class);
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Aqui estamos realizando un inflate a splasscreen_fragment
+		View view = inflater.inflate(R.layout.splashscreen_fragment, container, false);
+		return view;
+	}
 }
