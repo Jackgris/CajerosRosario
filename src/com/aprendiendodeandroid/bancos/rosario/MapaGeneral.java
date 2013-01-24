2926034ffb44caa4072ca354059bc0a0154cf481
jackgris <jackgris2@gmail.com>
2013-01-24 14:22:14 -0300

    separando totalmente la funcionalidad del los listener de ubicacion con la del mapa, asi se va a poder ampliar mas la app

--- src/com/aprendiendodeandroid/bancos/rosario/MapaGeneral.java
+++ src/com/aprendiendodeandroid/bancos/rosario/MapaGeneral.java
@@ -1,20 +1,16 @@
 package com.aprendiendodeandroid.bancos.rosario;
 
+import com.aprendiendodeandroid.bancos.rosario.utiles.LocationListenerGPS;
+import com.aprendiendodeandroid.bancos.rosario.utiles.LocationListenerNetwork;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import android.content.Intent;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
+
+import android.content.Context;
 import android.location.LocationManager;
 import android.location.LocationProvider;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.util.Log;
-import android.widget.Toast;
 
 /**
  * Esta clase la vamos a utilizar para manejar nuestra ubicacion y la de diferentes marcadores
@@ -25,145 +21,81 @@ import android.widget.Toast;
  */
 public class MapaGeneral extends android.support.v4.app.FragmentActivity {
 
-	private GoogleMap mapa;
+	public static GoogleMap mapa;
 	static final LatLng ROSARIO = new LatLng(-32.962, -780.662);
 	final float posicionzoomgeneral = 17;
 	LocationManager locationManager;
 	Marker actual;
-	LocationListener locationListenerNET;
-	LocationListener locationListenerGPS;
+	private LocationListenerNetwork locationListenerNET = new LocationListenerNetwork();
+	private LocationListenerGPS locationListenerGPS = new LocationListenerGPS();
+	public static Context context;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mapageneral);
+		context = getApplicationContext();
 		
-		locationManager = (LocationManager)this.getSystemService(getApplicationContext().LOCATION_SERVICE);
-		locationListenerGPS =
-		        new LocationListener() {
-		        public void onLocationChanged(Location location) {
-		            // aca vamos a realizar una accion al tomar un nuevo valor
-		            float lat = (float) (location.getLatitude());
-		            float lng = (float) (location.getLongitude());
-		            LatLng latLng = new LatLng(lat, lng);
-		            actual = mapa.addMarker(new MarkerOptions().position(latLng).title("Rosario"));
-		            Log.d("Localizacion", "Valor: " + location.toString() + " latitud: " + lat +
-		                      " logitud: " + lng + "Valor al mapa" + latLng);
-		        }
-		        public void onStatusChanged(String s, int i, Bundle bundle) {
-		 
-		        }
-		        public void onProviderEnabled(String provider) {
-		            // vamos a intentar cambiar con otros proveedores
-		            Log.d("Localizacion", "Esta habilitado el proveedor " + provider);
-		        }
-		        public void onProviderDisabled(String provider) {
-		          // vamos a intentar cambiar con otros proveedores
-		          Log.d("Localizacion", "Esta desabilitado el proveedor");
-		          Toast.makeText(getApplicationContext(), "El proveedor de ubicacion " + provider
-		                  + " esta desabilitado", Toast.LENGTH_SHORT).show();
-		          Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-		          startActivity(settingsIntent);
-		        }
-		      };
-        locationListenerNET = new LocationListener() {
-		        public void onLocationChanged(Location location) {
-		          // aca vamos a realizar una accion al tomar un nuevo valor
-		          float lat = (float) (location.getLatitude());
-		          float lng = (float) (location.getLongitude());
-		          LatLng latLng = new LatLng(lat, lng);
-		          actual = mapa.addMarker(new MarkerOptions().position(latLng).title("Rosario"));
-
-		          Log.d("Localizacion", "Valor: " + location.toString() + " latitud: " + lat +
-		                  " logitud: " + lng + "Valor al mapa" + latLng);		          
-		        }
-		        public void onStatusChanged(String s, int i, Bundle bundle) {
-		 
-		        }
-		        public void onProviderEnabled(String provider) {
-		           // vamos a intentar cambiar con otros proveedores
-		           Log.d("Localizacion", "Esta habilitado el proveedor");
-		        }
-		        public void onProviderDisabled(String provider) {
-		           // vamos a intentar cambiar con otros proveedores
-		           Log.d("Localizacion", "Esta desabilitado el proveedor");
-		           Toast.makeText(getApplicationContext(), "El proveedor de ubicacion " + provider
-		                   + " esta desabilitado", Toast.LENGTH_SHORT).show();
-		           Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-		              startActivity(settingsIntent);
-		        }
-		      };
+		getApplicationContext();
+        locationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
 		
-		mapa = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.mapa)).getMap();
-				
-		init();
+		mapa = (((SupportMapFragment)getSupportFragmentManager().
+		        findFragmentById(R.id.mapa)).getMap());
+		iniciamosLaEscucha();
     }
-	/** esta criteria va a tener menos precision, consumir menos energia, y un menor costo */
-	public static Criteria createCoarseCriteria() {
-	 
-	  Criteria c = new Criteria();
-	  c.setAccuracy(Criteria.ACCURACY_COARSE);
-	  c.setAltitudeRequired(false);
-	  c.setBearingRequired(false);
-	  c.setSpeedRequired(false);
-	  c.setCostAllowed(true);
-	  c.setPowerRequirement(Criteria.POWER_HIGH);
-	  return c;
-	}
-	/** esta criteria va a tener una mayor precision, consumir mas energia y un costo mayor*/
-	public static Criteria createFineCriteria() {
-	 
-	  Criteria c = new Criteria();
-	  c.setAccuracy(Criteria.ACCURACY_FINE);
-	  c.setAltitudeRequired(false);
-	  c.setBearingRequired(false);
-	  c.setSpeedRequired(false);
-	  c.setCostAllowed(true);
-	  c.setPowerRequirement(Criteria.POWER_HIGH);
-	  return c;
-	}
 	/** 
 	 hay que asegurarse de usar esta funcion en el hilo principal, y no en un hilo en background 
 	 hay que asegurarse de remover las actualizaciones cuando no se use mas
 	*/
-	public void init(){
+	public void iniciamosLaEscucha(){
 	  // baja precision
 	  LocationProvider low=
-	    locationManager.getProvider(locationManager.getBestProvider(createCoarseCriteria(),false));
+	    locationManager.getProvider(locationManager.getBestProvider(LocationListenerNetwork.
+	            crearCriteriaRed(),false));
 	 
 	  // alta precision
 	  LocationProvider high=
-	    locationManager.getProvider(locationManager.getBestProvider(createFineCriteria(), false));
+	    locationManager.getProvider(locationManager.getBestProvider(LocationListenerGPS.
+	            crearCriteriaGPS(), false));
 	 
 	  // usamos el proveedor de baja precision... y le ponemos el listener para que lo actualice
 	  locationManager.requestLocationUpdates(low.getName(), 0, 0f, locationListenerNET);
 
 	  // usamos el proveedor que tiene mayor precision... le agregamos el listener para actualizar
-	  locationManager.requestLocationUpdates(high.getName(), 0, 0f, locationListenerGPS);			  
-
+	  locationManager.requestLocationUpdates(high.getName(), 0, 0f, locationListenerGPS);
+	  
+	  // TODO hay que verificar que esto funcione correctamente
+	  if (locationListenerGPS.getMapa() != null) {
+	      mapa = locationListenerGPS.getMapa();
+	  }
+	  else if (locationListenerNET.getMapa() != null) {
+	      mapa = locationListenerNET.getMapa();
+	  }
 	}
-		
+	/**
+	 * Vamos a detener la escucha de nuestro listener y todo tipo de actualizacion	
+	 */
 	@Override
 	protected void onStop() {
-		// aca vamos a remover todas las actualizaciones
 		super.onStop();
 		locationManager.removeUpdates(locationListenerGPS);
 		locationManager.removeUpdates(locationListenerNET);
 	}
-	
+	/**
+	 * Vamos a detener la escucha de nuestro listener y todo tipo de actualizacion 
+	 */
 	@Override
 	protected void onPause() {
-		// aca vamos a remover todas las actualizaciones
 		super.onPause();
 		locationManager.removeUpdates(locationListenerGPS);
 		locationManager.removeUpdates(locationListenerNET);
 	}
-	
+	/**
+	 * Volvemos a arrancar las actualizaciones
+	 */
 	@Override
 	protected void onResume() {
-		// TODO Auto-generated method stub
 		super.onResume();
-		init();
-	}
-	
+		iniciamosLaEscucha();
+	}  
 }
--- src/com/aprendiendodeandroid/bancos/rosario/utiles/LocationListenerNetwork.java
+++ src/com/aprendiendodeandroid/bancos/rosario/utiles/LocationListenerNetwork.java
@@ -1,84 +1,44 @@
 package com.aprendiendodeandroid.bancos.rosario.utiles;
 
+import com.aprendiendodeandroid.bancos.rosario.MapaGeneral;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
+import android.util.Log;
 
 public class LocationListenerNetwork implements LocationListener{
-    long fecha_hora_anterior = 0;
-//    private Posicion posicion = Posicion.getInstance();
-    private String latitud = "";
-    private String longitud = "";
+    private Marker actual;
+    private GoogleMap mapa;
     
     public void onLocationChanged(Location location) {
-        // Obtengo la fecha y hora actual
-        java.util.Date fecha_hora_actual = new java.util.Date();
-        
-        // Si es la primera vez que entro no tengo la fecha y hora anterior
-        // por eso le doy un valor inicial
-        if(fecha_hora_anterior == 0) {
-            fecha_hora_anterior = fecha_hora_actual.getTime();
-        } else {
-            // Resto la fecha y hora actual con la anterior
-            // paso el resultado a segundos
-            float dif_tiempo = (fecha_hora_actual.getTime() - fecha_hora_anterior) / 1000;
-            // ahora paso el la diferencia de tiempo en horas
-            dif_tiempo = dif_tiempo / 3600;
-            
-            // Llamo a la función obtener_velocidad y guardo su resultado en
-            // la variable pública velocidad
-//            posicion.velocidad = posicion.obtener_velocidad(location.getLatitude(),
-//                    location.getLongitude(), posicion.latitud_movil, posicion.longitud_movil,
-//                    dif_tiempo);
+        // aca vamos a realizar una accion al tomar un nuevo valor
+        float lat = (float) (location.getLatitude());
+        float lng = (float) (location.getLongitude());
+        LatLng latLng = new LatLng(lat, lng);
+        mapa = MapaGeneral.mapa;
+        actual = mapa.addMarker(new MarkerOptions().position(latLng).title("Rosario"));
+
+        Log.d("Localizacion", "Valor: " + location.toString() + " latitud: " + lat +
+              " logitud: " + lng + "Valor al mapa" + latLng);                 
         }           
-        // Actualizo la latitud y longitud para enviar al servidor
-        latitud = String.valueOf(location.getLatitude());
-        longitud = String.valueOf(location.getLongitude());
-        
-        // Si tengo la posición anterior del movil obtengo su rumbo
-//        if(posicion.latitud_movil != 0 && posicion.longitud_movil != 0) {
-//            // Obtengo el rumbo actual
-//            posicion.rumbo = posicion.obtener_angulo_entre_dos_puntos( posicion.latitud_movil,
-//                    posicion.longitud_movil, location.getLatitude(), location.getLongitude());
-        }           
-        // Guardo la latitud y longitud en el singleton Posicion
-//        posicion.latitud_movil = location.getLatitude();
-//        posicion.longitud_movil = location.getLongitude();
-        
-        // obtengo la presicion del GPS
-//        posicion.presicion = location.getAccuracy();
-        
-        // Actualizo el valor de la fecha y hora anterior
-//        fecha_hora_anterior = fecha_hora_actual.getTime();
-//    }
     public void onStatusChanged(String provider, int status, Bundle extras) {
     }
 
-    public void onProviderDisabled(String s) {
-        // Si pierdo senial de GPS seteo la latitud y la longitud a vaci�o
-        latitud = "";
-        longitud = "";
-        // Seteo en 0.0 la posicion del movil
-//        posicion.latitud_movil = 0.0;
-//        posicion.longitud_movil = 0.0;
+    public void onProviderDisabled(String provider) {
+        // vamos a intentar cambiar con otros proveedores
+        Log.d("Localizacion", "Esta desabilitado el proveedor");
     }
     public void onProviderEnabled(String s) {
+        // vamos a intentar cambiar con otros proveedores
+        Log.d("Localizacion", "Esta habilitado el proveedor");
     }
-    
-    // getter and setter para actualizar los valores
-    public String getLatitud() {
-        return latitud;
-    }
-    public void setLatitud(String latitud) {
-        this.latitud = latitud;
-    }
-    public String getLongitud() {
-        return longitud;
-    }
-    public void setLongitud(String longitud) {
-        this.longitud = longitud;
-    }
+
     /** esta criteria va a tener menos precision, consumir menos energia, y un menor costo */
     public static Criteria crearCriteriaRed() {
      
@@ -90,5 +50,20 @@ public class LocationListenerNetwork implements LocationListener{
       c.setCostAllowed(true);
       c.setPowerRequirement(Criteria.POWER_HIGH);
       return c;
+    }
+    public Marker getActual() {
+        return actual;
+    }
+
+    public void setActual(Marker actual) {
+        this.actual = actual;
+    }
+
+    public GoogleMap getMapa() {
+        return mapa;
+    }
+
+    public void setMapa(GoogleMap mapa) {
+        this.mapa = mapa;
     }
 }
